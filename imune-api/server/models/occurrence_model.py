c66082ce940dbde5ed65cc7766dd332eef2aa6c5
Anderson Rocha <anderson-rocha@outlook.com>
2020-05-04 20:23:21 -0300

    Fix document type to accept string.

--- imune-api/server/models/occurrence_model.py
+++ imune-api/server/models/occurrence_model.py
@@ -1,14 +1,13 @@
 from uuid import UUID
 
 from server.models.base_model import ApiBaseModel
-from server.models.vaccinate_model import DocumentType
 
 
 class Occurrence(ApiBaseModel):
     address: str
     batch: UUID
     document: str
-    document_type: DocumentType
+    document_type: str
     vaccine: str
     death: bool
     effects: str
